7c2c3243f2093096106cf2f1aea88f6a4fc279d1
Augusto Espinoza <aler1983@gmail.com>
2025-07-09 20:25:06 -0500

    se agrego XXXXXX,fixes #5

--- visualizacion_iris.py
+++ visualizacion_iris.py
@@ -10,7 +10,7 @@ print(df.head())
 
 # Gráfico 1: Cantidad de flores por especie
 df['species'].value_counts().plot(kind='bar')
-plt.title('Cantidad de flores por especie xxxxx')
+plt.title('Cantidad de flores por especie xxxxxx')
 plt.xlabel('Especie')
 plt.ylabel('Cantidad')
 plt.tight_layout()
